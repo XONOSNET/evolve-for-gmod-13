cadecf3929606ec74cd2cd1bf27beb81ac7d61ad
admin@northdegree.de <admin@northdegree.de@fec3e26a-cdbf-9ae1-bae3-18635c041c88>
2012-05-26 19:14:33 +0000

    Main Upload

---
+++ lua/autorun/client/ev_autorun.lua
@@ -0,0 +1,14 @@
+/*-------------------------------------------------------------------------------------------------------------------------
+	Clientside autorun file
+-------------------------------------------------------------------------------------------------------------------------*/
+
+// Set up evolve table
+evolve = {}
+
+// Requirements
+require( "glon" )
+
+// Load clientside files
+include( "ev_framework.lua" )
+include( "ev_menu/cl_menu.lua" )
+include( "ev_cl_init.lua" )
