197ae47b6254b08d6e96a151253e84193a5c754c
admin@northdegree.de <admin@northdegree.de@fec3e26a-cdbf-9ae1-bae3-18635c041c88>
2012-06-06 18:07:15 +0000

    - SQL Support eingebaut - Global-Chat auf MySQL umgerüstet

--- lua/ev_plugins/sv_globalchat.lua
+++ lua/ev_plugins/sv_globalchat.lua
@@ -1,55 +1,75 @@
 /*-------------------------------------------------------------------------------------------------------------------------
 	AGC Global Chat!!
 -------------------------------------------------------------------------------------------------------------------------*/
-GlobalChatOnline = 1
-EnableTime = 0
 local PLUGIN = {}
 PLUGIN.Title = "Global Chat"
 PLUGIN.Description = "Connect Server-chats"
 PLUGIN.Author = "Northdegree"
-PLUGIN.ChatCommand = "!g"
-PLUGIN.Usage = "<message> (Sends a Message to all Servers)"
-PLUGIN.Privileges = { "Toggle-Global-Chat"}
+PLUGIN.ChatCommand = "g"
+PLUGIN.Usage = "<message>"
+PLUGIN.Privileges = { "Toggle Global Chat", "Global Chat"}
+
+/*------------------ Variablen ----------------------*/
+GlobalChatOnline = 1
+EnableTime = tonumber(os.time())
+/*--------------------------------------------------*/
 
 function PLUGIN:Call( ply, args )
-	if not args then
-		if GlobalChatOnline == 1 then
-			GlobalChatOnline = 0
-			EnableTime = tonumber(os.time())
-			evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has", evolve.colors.red, " disabled", evolve.colors.white, " the Global Chat")
-			return
+	if not args[1] then
+		if ply:EV_HasPrivilege( "Toggle Global Chat" ) then
+			if GlobalChatOnline == 1 then
+				GlobalChatOnline = 0
+				EnableTime = 0
+				evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has", evolve.colors.red, " disabled", evolve.colors.white, " the Global Chat")
+				return
+			else
+				GlobalChatOnline = 1
+				EnableTime = tonumber(os.time())
+				evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has", evolve.colors.blue, " enabled", evolve.colors.white, " the Global Chat")
+				return
+			end
 		else
-			GlobalChatOnline = 1
-			EnableTime = tonumber(os.time())
-			evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has", evolve.colors.blue, " enabled", evolve.colors.white, " the Global Chat")
-			return
+			evolve:Notify( ply, evolve.colors.red, evolve.constants.notallowed )
 		end
 	else
-	
+		if ply:EV_HasPrivilege( "Toggle Global Chat" ) then
+			if GlobalChatOnline == 1 then
+				local unargs={unpack(args)}
+				local outputstring = table.concat(unargs, " ")
+				local data = evolve.ranks[ ply:EV_GetRank() ]
+				local color = data.Color
+				evolve:Notify( evolve.colors.white, "[AGC-Global] " , color, ply:Nick(), evolve.colors.white, ": "..outputstring)
+				local plyname = DB.MySQLDB:escape(ply:Nick())
+				local text = DB.MySQLDB:escape( outputstring )
+				DB.Query("INSERT INTO `globalchat` (`time`, `site`, `user`, `text`) VALUES ('"..os.time().."', 'Build', '"..plyname.."', '"..text.."');")
+			else
+				evolve:Notify( ply, evolve.colors.red, "The Global Chat is deactivated!" )
+			end
+		else
+			evolve:Notify( ply, evolve.colors.red, evolve.constants.notallowed )
+		end
 	end
 end
-
+/*
 hook.Add( "PlayerSay", "EV_PlayerChat", function( ply, txt )
 	if GlobalChatOnline == 1 then
-		local playername = ply:Nick()
-		print("lol-public.com/gmodfastdl/updateserver/globalchat/globalchatinsert.php?sent="..os.time().."&author="..playername.."&from=Build&text="..txt)
-		http.Get( "lol-public.com/gmodfastdl/updateserver/globalchat/globalchatinsert.php?sent="..os.time().."&author="..playername.."&from=Build&text="..txt, "", function( contins, sizeins )
-		print(";;Content: "..contins.."; Size"..sizeins..";;" ) 
-		end )
+		local plyname = DB.MySQLDB:escape(ply:Nick())
+		local text = DB.MySQLDB:escape(txt)
+		DB.Query("INSERT INTO `globalchat` (`time`, `site`, `user`, `text`) VALUES ('"..os.time().."', 'Build', '"..plyname.."', '"..text.."');")
 	end
 end )
+*/
+
 function RecieveGlobalChats()
 	if GlobalChatOnline == 1 then
-		http.Get( "lol-public.com/gmodfastdl/updateserver/globalchat/globalchatget.php?enabletime="..EnableTime.."&server=Build", "", function( cont, s )
-			if tostring(cont)=="" or s==0 then return end
-				glcgstrsplit1 = string.Explode("|---|",cont)
-				for _,val in pairs(glcgstrsplit1) do
-					if val != "" then
-						glcgstrsplit = string.Explode("|--|",val)
-						evolve:Notify( evolve.colors.white, "["..glcgstrsplit[1].."]", evolve.colors.blue, glcgstrsplit[2], evolve.colors.white, ": "..glcgstrsplit[3].."")
-					end
+		DB.Query("SELECT * FROM `globalchat` WHERE time>"..EnableTime.." AND sentBuild=0 and site!='Build' ORDER BY id DESC LIMIT 0 , 5;", function(results)
+			if results then
+				for k,v in pairs(results) do
+					evolve:Notify( evolve.colors.white, "["..v.site.."] ", evolve.colors.blue, v.user, evolve.colors.white, ": "..v.text )
+					DB.Query("UPDATE `globalchat` SET sentBuild=1 WHERE id="..v.id..";")
 				end
-		end )
+			end
+		end)
 	end
 end
 timer.Create("RecGlobalChats",2,0,RecieveGlobalChats)
