2eacce737b0010f604d554940ac15a7b7eb086f2
admin@northdegree.de <admin@northdegree.de@fec3e26a-cdbf-9ae1-bae3-18635c041c88>
2012-06-06 00:24:13 +0000

    Das abrufen der Chats funktioniert nun... es fehlt noch das eintragen...

--- lua/ev_plugins/sv_globalchat.lua
+++ lua/ev_plugins/sv_globalchat.lua
@@ -1,46 +1,53 @@
 /*-------------------------------------------------------------------------------------------------------------------------
 	AGC Global Chat!!
 -------------------------------------------------------------------------------------------------------------------------*/
-local GlobalChatOnline = 0
-local EnableTime
+GlobalChatOnline = 1
+EnableTime = 0
 local PLUGIN = {}
 PLUGIN.Title = "Global Chat"
 PLUGIN.Description = "Connect Server-chats"
 PLUGIN.Author = "Northdegree"
-PLUGIN.ChatCommand = "globalchat"
-PLUGIN.Usage = ""
+PLUGIN.ChatCommand = "!g"
+PLUGIN.Usage = "<message> (Sends a Message to all Servers)"
 PLUGIN.Privileges = { "Toggle-Global-Chat"}
 
 function PLUGIN:Call( ply, args )
-	if GlobalChatOnline then
-		GlobalChatOnline = 0
-		EnableTime = tonumber(os.time())
-		evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has", evolve.colors.red, " disabled", evolve.colors.white, " the Global Chat")
-		return
+	if not args then
+		if GlobalChatOnline == 1 then
+			GlobalChatOnline = 0
+			EnableTime = tonumber(os.time())
+			evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has", evolve.colors.red, " disabled", evolve.colors.white, " the Global Chat")
+			return
+		else
+			GlobalChatOnline = 1
+			EnableTime = tonumber(os.time())
+			evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has", evolve.colors.blue, " enabled", evolve.colors.white, " the Global Chat")
+			return
+		end
 	else
-		GlobalChatOnline = 1
-		EnableTime = tonumber(os.time())
-		evolve:Notify( evolve.colors.blue, ply:Nick(), evolve.colors.white, " has", evolve.colors.green, " enabled", evolve.colors.white, " the Global Chat")
-		return
+	
 	end
 end
 
-function GlobalChatHook( playerindex, playername, text, messagetype )
-	if GlobalChatOnline then
-		http.Get( "lol-public.com/gmodfastdl/updateserver/globalchat/globalchatinsert.php?sent="..tonumber(os.time()).."&author="..playername.."&from=Build&text="..text.."", "", function( c, s )end )
+hook.Add( "PlayerSay", "EV_PlayerChat", function( ply, txt )
+	if GlobalChatOnline == 1 then
+		local playername = ply:Nick()
+		print("lol-public.com/gmodfastdl/updateserver/globalchat/globalchatinsert.php?sent="..os.time().."&author="..playername.."&from=Build&text="..txt)
+		http.Get( "lol-public.com/gmodfastdl/updateserver/globalchat/globalchatinsert.php?sent="..os.time().."&author="..playername.."&from=Build&text="..txt, "", function( contins, sizeins )
+		print(";;Content: "..contins.."; Size"..sizeins..";;" ) 
+		end )
 	end
-	return
-end
-// Hook the event.
-hook.Add( "ChatText", "Evolve-ChatHook", GlobalChatHook );
+end )
 function RecieveGlobalChats()
-	if GlobalChatOnline then
-		http.Get( "lol-public.com/gmodfastdl/updateserver/globalchat/globalchatget.php?enabletime="..EnableTime.."&server=Build", "", function( c, s )
-			if not c then return end
-				glcgstrsplit1 = string.Explode(s, "New12String32")
+	if GlobalChatOnline == 1 then
+		http.Get( "lol-public.com/gmodfastdl/updateserver/globalchat/globalchatget.php?enabletime="..EnableTime.."&server=Build", "", function( cont, s )
+			if tostring(cont)=="" or s==0 then return end
+				glcgstrsplit1 = string.Explode("|---|",cont)
 				for _,val in pairs(glcgstrsplit1) do
-					glcgstrsplit = string.Explode(val, "|--|")
-					evolve:Notify( evolve.colors.white, "["..glcgstrsplit[0].."]", evolve.colors.blue, glcgstrsplit[1], evolve.colors.white, ": "..glcgstrsplit[2].."")
+					if val != "" then
+						glcgstrsplit = string.Explode("|--|",val)
+						evolve:Notify( evolve.colors.white, "["..glcgstrsplit[1].."]", evolve.colors.blue, glcgstrsplit[2], evolve.colors.white, ": "..glcgstrsplit[3].."")
+					end
 				end
 		end )
 	end
